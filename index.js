c8e09c7097832d901b87acb3e11dc9ae28d581cf
Armando Rojas <rojas.armani@gmail.com>
2021-05-25 13:07:30 -0400

    commit tree 8fcd7b5c75a3c3ee5591b33f1ac130813c395189

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(array, event) {
+
+    const thanksArray = [];
+
+    for (let i=0; i < array.length; i++) {
+        thanksArray.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return thanksArray;
+}
+
+function countDown(number) {
+    let i = number;
+
+    while(i >= 0) {
+        console.log(i);
+        i--
+    }
+}
